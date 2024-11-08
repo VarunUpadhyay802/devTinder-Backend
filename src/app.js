d56107ddeaeebe1f4c95a53d8068aee039ecfc03
Varun Upadhyay <varun.802.vu@gmail.com>
2024-11-08 08:37:51 +0530

    route handlers blueprint

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require("express")
 
 const app = express();
 const port = 4000
-
-// handling the requests
-app.use("/test",(req,res)=>{
-    res.send("hello from the server")
+const {adminAuth} = require("./middlewares/auth")
+//generally we use app.use for middlewares
+app.use("/admin" ,adminAuth)
+app.get("/admin/getAllData",(req,res,next)=>{
+    //so here the express will come only when the auth is completed
+    res.send("response sent ")
 })
 
-app.listen(4000,()=>{
+app.listen(4000, () => {
     console.log(`server is running on port ${port}`)
 })
