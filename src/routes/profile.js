8ca714f277a25ae08630e05dbad5d5a69dae8400
Varun Upadhyay <varun.802.vu@gmail.com>
2024-11-16 16:50:28 +0530

    forgot password api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { userAuth } = require('../middlewares/auth');
 const User = require('../models/user');
 const { validateEditProfileData } = require('../utils/validation');
-
+const validator = require('validator')
 const profileRouter = express.Router();
-
+const bcrypt = require('bcrypt')
 
 profileRouter.get("/view", userAuth, async (req, res) => {
 
@@ -18,7 +18,7 @@ profileRouter.get("/view", userAuth, async (req, res) => {
 })
 
 profileRouter.patch("/edit", userAuth, async (req, res) => {
-  
+
     try {
         if (!validateEditProfileData(req)) {
             throw new Error("Invalid Edit request");
@@ -26,14 +26,14 @@ profileRouter.patch("/edit", userAuth, async (req, res) => {
         //auth middleware has attached the user with this
         const loggedInuser = req.user;
         // loggedInuser.firstName = req.body.firstName
-        Object.keys(req.body).forEach((key)=>(
+        Object.keys(req.body).forEach((key) => (
             loggedInuser[key] = req.body[key]
         ))
         // console.log(user);
         await loggedInuser.save();
         res.json({
-            message : `${loggedInuser.firstName}, your Updation successful` ,
-            data : loggedInuser
+            message: `${loggedInuser.firstName}, your Updation successful`,
+            data: loggedInuser
         })
 
 
@@ -44,9 +44,28 @@ profileRouter.patch("/edit", userAuth, async (req, res) => {
 
 })
 
-profileRouter.patch("/password" , (req,res)=>{
+profileRouter.patch("/password", userAuth, async (req, res) => {
     //forgot password api 
-    //first user should know that 
+    //first user should be logged in 
+    //logged in user password update to the req.body.password
+
+    try {
+        // if (!validator.isPassword(loggedInuser.password)) {
+        //     throw new Error("Password is not strong enough")
+        // }
+        //if strong enough 
+        const loggedInuser = req.user;
+        //password hashing of req.body
+        const passwordHash = await bcrypt.hash(req.body.password, 10);
+        //updating the hash in db 
+        loggedInuser.password = passwordHash;
+        await loggedInuser.save();
+        res.json({
+            message : "Password updated successfully"
+        })
+    } catch (error) {
+        res.status(500).send("Internal Server error")
+    }
 })
 
 module.exports = profileRouter
